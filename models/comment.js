a2caced53430f7d925ba174c90f66cee26c3fdbb
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-09-17 11:01:48 -0700

    added comment model

---
+++ models/comment.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+
+var commentSchema = new mongoose.Schema({
+	firstName: {
+    type: String,
+    minlength: 1,
+    maxlength: 99
+  },
+  lastName: {
+    type: String,
+    minlength: 1,
+    maxlength: 99
+  },
+  description: {
+  	type: String,
+  	required: true,
+  	minlength: 1,
+  	maxlength: 250
+  },
+  star: {
+  	type: Number,
+  	required: true
+  }
+});
+
+commentSchema.set('toJSON', {
+  transform: function(doc, comment, options) {
+    var returnJson = {
+      id: comment._id,
+      firstName: comment.firstName,
+      lastName: comment.lastName,
+      description: comment.description,
+      star: comment.star
+    };
+    return returnJson;
+  }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
